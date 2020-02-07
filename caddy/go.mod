eef1800fe51f9f391d68eb45ae615ea5e757d63b
Daniel Lewan <vision360.daniel@gmail.com>
2020-02-07 18:17:06 +0100

    add basic caddy configuration

---
+++ caddy/go.mod
@@ -0,0 +1,13 @@
+module caddy
+
+go 1.13
+
+require (
+	github.com/BTBurke/caddy-jwt v3.7.1+incompatible
+	github.com/caddyserver/caddy v1.0.4
+	github.com/captncraig/cors v0.0.0-20190703115713-e80254a89df1
+	github.com/nicolasazrak/caddy-cache v0.3.4
+	github.com/pquerna/cachecontrol v0.0.0-20180517163645-1555304b9b35 // indirect
+	github.com/tarent/loginsrv v1.3.1
+	github.com/xuqingfeng/caddy-rate-limit v1.6.6
+)
---
+++ caddy/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/caddyserver/caddy/caddy/caddymain"
+
+	// plug in plugins here, for example:
+	// _ "import/path/here"
+	_ "github.com/BTBurke/caddy-jwt"
+	_ "github.com/captncraig/cors"
+	_ "github.com/nicolasazrak/caddy-cache"
+	_ "github.com/tarent/loginsrv/caddy"
+	_ "github.com/xuqingfeng/caddy-rate-limit"
+)
+
+func main() {
+	// optional: disable telemetry
+	caddymain.EnableTelemetry = false
+	caddymain.Run()
+}
